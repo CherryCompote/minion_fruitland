56b1ce1792f6a18590af619924710b31808b7ec3
cherrycompote <veryverycindy@gmail.com>
2025-02-27 19:14:28 +0800

    Get user data and select location

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-print("hello world")
+name = input("What is your minion name?")
+experience_level = int(input("What is your experience level?"))
+mission_num = int(input("What is your mission number?"))
 
-print("I like minion shaped bacon")
+print(f"Hi {name}! Your experience level is {experience_level}, and your mission number today is {mission_num}.")
 
-print("cherry compote")
+if mission_num > 0 and mission_num <= 25:
+    print("This is an easy mission.")
+elif mission_num > 25 and mission_num <= 50:
+    print("This is a normal mission.")
+elif mission_num > 50 and mission_num <= 75:
+    print("This is a hard mission!")
+else:
+    print("Whew! This is a EXTREME mission!")
 
-print("do you like bacon?")
+destination = input("Where do you want to explore?")
 
-# the code above is asking the player
+if destination == "banana village":
+    print("Okay! Get ready for banana cream pies!")
+    experience_level += 10
+elif destination == "kiwi forest":
+    print("Ooo! Get ready for Kiwi bird attacks!")
+    experience_level += 20
+elif destination == "pineapple mountain":
+    print("Yikes! Get ready to meet the pineapple cave monster!")
+    experience_level += 30
+else:
+    print("Uh oh! This location isn't in our maps, be careful!")
+    experience_level += 50
