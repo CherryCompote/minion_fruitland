a50c3863bb7e64b651abfcf0286b3034ef4af9df
cherrycompote <veryverycindy@gmail.com>
2025-06-18 19:54:00 +0800

    new characters

--- monster_battle.py
+++ monster_battle.py
@@ -44,6 +44,26 @@ list_of_warriors = [
     ]
     },
     {
+    "name": "Taylor",
+    "full_name": "Taylor Swift",
+    "speed": 80,
+    "hp": 110, #Health points
+    "attacks": [
+        {
+            "name": "mic throw",
+            "ap": 20, #Attack power
+            "pp": 5, #How many times the player can use it
+            "accuracy": 75 #how likely it is to hit the enemy
+        },
+        {
+            "name": "magic song",
+            "ap": 10,
+            "pp": 10,
+            "accuracy": 85
+        }
+    ]
+    },
+    {
     "name": "Mario",
     "full_name": "Mario",
     "speed": 85,
@@ -64,22 +84,122 @@ list_of_warriors = [
     ]
     },
     {
+    "name": "Peach",
+    "full_name": "Princess Peach",
+    "speed": 60,
+    "hp": 120, #Health points
+    "attacks": [
+        {
+            "name": "toad attack",
+            "ap": 10, #Attack power
+            "pp": 5, #How many times the player can use it
+            "accuracy": 95 #how likely it is to hit the enemy
+        },
+        {
+            "name": "umbrella wack",
+            "ap": 20,
+            "pp": 8,
+            "accuracy": 75
+        }
+    ]
+    },
+    {
     "name": "George",
     "full_name": "George Lo",
     "speed": 80,
     "hp": 110, #Health points
     "attacks": [
         {
-            "name": "Metal backpack",
+            "name": "metal backpack",
             "ap": 15, #Attack power
             "pp": 5, #How many times the player can use it
             "accuracy": 85 #how likely it is to hit the enemy
         },
         {
-            "name": "ID swing",
+            "name": "id swing",
             "ap": 5,
             "pp": 15,
             "accuracy": 90
+        }
+    ]
+    },
+    {
+    "name": "Mickey",
+    "full_name": "Mickey Mouse",
+    "speed": 80,
+    "hp": 90, #Health points
+    "attacks": [
+        {
+            "name": "toodles",
+            "ap": 5, #Attack power
+            "pp": 15, #How many times the player can use it
+            "accuracy": 90 #how likely it is to hit the enemy
+        },
+        {
+            "name": "sorceror blast",
+            "ap": 20,
+            "pp": 5,
+            "accuracy": 75
+        }
+    ]
+    },
+    {
+    "name": "Minnie",
+    "full_name": "Minnie Mouse",
+    "speed": 75,
+    "hp": 90, #Health points
+    "attacks": [
+        {
+            "name": "toodles",
+            "ap": 5, #Attack power
+            "pp": 15, #How many times the player can use it
+            "accuracy": 90 #how likely it is to hit the enemy
+        },
+        {
+            "name": "bow throw",
+            "ap": 15,
+            "pp": 8,
+            "accuracy": 80
+        }
+    ]
+    },
+    {
+    "name": "Harry",
+    "full_name": "Harry Potter",
+    "speed": 90,
+    "hp": 90, #Health points
+    "attacks": [
+        {
+            "name": "avada kedavra",
+            "ap": 30, #Attack power
+            "pp": 1, #How many times the player can use it
+            "accuracy": 95 #how likely it is to hit the enemy
+        },
+        {
+            "name": "stupefy",
+            "ap": 5,
+            "pp": 10,
+            "accuracy": 80
+        }
+    ]
+    },
+    {
+    "name": "Cleopatra",
+    "full_name": "Cleopatra",
+    "speed": 80,
+    "hp": 120, #Health points
+    "attacks": [
+        {
+            "name": "sandstorm",
+            "ap": 15, #Attack power
+            "pp": 5, #How many times the player can use it
+            "accuracy": 90 #how likely it is to hit the enemy
+        },
+        {
+            "name": "poison dart",
+            "ap": 10,
+            "pp": 10,
+            "accuracy": 85
         }
     ]
     }
